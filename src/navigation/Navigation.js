3309816b02d593160894973e5bbaaac5251a84be
ABHINAV JHA <75615092+ABHINAV-JHA-27@users.noreply.github.com>
2022-08-18 10:31:03 +0530

    added necessary directories

---
+++ src/navigation/Navigation.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navigation = () => {
+    return (
+        <View>
+            <Text>Navigation</Text>
+        </View>
+    );
+};
+
+export default Navigation;
